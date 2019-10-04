dfdba3852c1bb7d059b3e688e8fb32bc268ecce0
LarrySchultheis <43074882+LarrySchultheis@users.noreply.github.com>
2019-10-03 23:33:13 -0400

    Initial project creation

---
+++ testMVC/MVCApp/MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCApp.Models;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            int a = 0;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            int a = 0;
+            return View("Index");
+        }
+
+        public IActionResult DataEntry()
+        {
+            return View("DataEntry");
+        }
+
+        public IActionResult HomePage()
+        {
+            return View("HomePage");
+        }
+
+        [HttpPost]
+        public IActionResult VerifyLogin(User user)
+        {
+
+            return View("HomePage", user);
+        }
+
+        [HttpPost]
+        public IActionResult PostEntry()
+        {
+            return View("HomePage");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ testMVC/MVCApp/MVCApp/Models/DummyData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApp.Models
+{
+    public class DummyData
+    {
+        public string JobType { get; set; }
+        public string tripNo { get; set; }
+        public string stageNo { get; set; }
+        public string EmplName { get; set; }
+        public string StartTime { get; set; }
+
+        public DummyData(string JobType, string tripNo, string stageNo, string EmplName, string startTime)
+        {
+            this.JobType = JobType;
+            this.tripNo = tripNo;
+            this.stageNo = stageNo;
+            this.EmplName = EmplName;
+            this.StartTime = startTime;
+        }
+    }
+}
